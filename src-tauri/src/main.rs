8092c199732854809f24eec1b86ea5dbf831855a
Giuseppe Barillari <joe2k01dev@gmail.com>
2023-11-05 14:04:38 +0000

    Initial POC SVG generation
    Signed-off-by: Giuseppe Barillari <joe2k01dev@gmail.com>

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,64 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use svg::node::element::path::Data;
+use svg::node::element::Group;
+use svg::node::element::Path;
+use svg::Document;
+
+fn draw_processor(r: i32, c: i32) -> Group {
+    let data = Data::new()
+        .move_to((150 * r, 150 * c))
+        .line_by((75, 0))
+        .line_by((25, 25))
+        .line_by((0, 75))
+        .line_by((-100, 0))
+        .line_by((0, -100))
+        .move_by((75, 0))
+        .line_by((0, -25))
+        .line_by((50, 0))
+        .line_by((0, 50))
+        .line_by((-25, 0));
+
+    let path = Path::new()
+        .set("fill", "none")
+        .set("stroke", "black")
+        .set("stroke-width", 3)
+        .set("fill-rule", "evenodd")
+        .set("stroke-linecap", "butt")
+        .set("d", data);
+
+    return Group::new()
+        .set("id", format!("{},{}", r + 1, c + 1))
+        .add(path);
+}
+
+#[tauri::command]
+fn get_procesor_info(r: i32, c: i32) -> String {
+    return format!("You are hovering on processor {},{}", r + 1, c + 1);
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_svg() -> String {
+    let rows = 10;
+    let cols = 10;
+
+    // Create document
+    let mut document = Document::new().set("viewbox", (0, 0, rows * 150, cols * 150));
+
+    for c in 0..cols {
+        for r in 0..rows {
+            document = document.add(draw_processor(r, c));
+        }
+    }
+
+    return document.to_string();
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_svg, get_procesor_info])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,44 @@
-import { createSignal } from "solid-js";
-import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/tauri";
+import { onMount } from "solid-js";
 import "./App.css";
 
 function App() {
-  const [greetMsg, setGreetMsg] = createSignal("");
-  const [name, setName] = createSignal("");
+  let render: HTMLDivElement | undefined;
 
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name: name() }));
-  }
+  onMount(() => {
+    invoke("get_svg")
+      .then((res) => {
+        if (render && typeof res === "string") {
+          const parser = new DOMParser();
+          const svg = parser.parseFromString(res, "image/svg+xml");
+          svg.documentElement.childNodes.forEach((node, i) => {
+            node.addEventListener("mouseenter", () => {
+              const id = (node as HTMLElement).id
+                .split(",")
+                .map((n) => parseInt(n) - 1);
+              invoke("get_procesor_info", { r: id[0], c: id[1] })
+                .then((res) => console.log(res))
+                .catch(console.error);
+            });
 
+            node.addEventListener("mouseleave", () => {
+              console.log(`Left node ${i}`);
+            });
+          });
+          render.appendChild(svg.documentElement);
+        } else
+          throw new Error(
+            `Canvas is undefined: ${render === undefined}. Res is string: ${
+              typeof res === "string"
+            }`
+          );
+      })
+      .catch(console.log);
+  });
   return (
     <div class="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div class="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={logo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and Solid logos to learn more.</p>
-
-      <form
-        class="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg()}</p>
+      <div class="side-panel"></div>
+      <div class="render" ref={render}></div>
     </div>
   );
 }
